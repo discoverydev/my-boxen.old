381085bcaab9949609e2659799f78d602e521195
discoverydev <adsdiscoveryteam@pillartechnology.com>
2015-05-26 15:21:07 -0400

    adding ddaugher

---
+++ modules/people/manifests/krsmes/config/gitconfig.pp
@@ -0,0 +1,31 @@
+class people::krsmes::gitconfig (
+    $my_homedir   = $people::krsmes::params::my_homedir,
+    $my_sourcedir = $people::krsmes::params::my_sourcedir,
+    $my_username  = $people::krsmes::params::my_username,
+    $my_email     = $people::krsmes::params::my_email
+    ){
+  
+  git::config::global {
+    'user.name':    value => 'Kevin Smith';
+    'user.email':   value => 'ksmith@pillartechnology.com';
+    'color.ui':     value => 'auto';
+    'github.user':  value => 'ksmith';
+    'push.default': value => 'simple';
+    'alias.a':      value => 'add';
+    'alias.aa':     value => 'add -A';
+    'alias.bv':     value => 'branch -avv';
+    'alias.co':     value => 'checkout';
+    'alias.c':      value => 'commit';
+    'alias.cm':     value => 'commit -m';
+    'alias.ca':     value => 'commit -a';
+    'alias.cam':    value => 'commit -a -m';
+    'alias.d':      value => 'diff';
+    'alias.ds':     value => 'diff --stat';
+    'alias.l':      value => 'log --graph --pretty=format:\'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset\' --abbrev-commit --date=relative';
+    'alias.l1':     value => 'log --pretty=oneline';
+    'alias.s':      value => 'status';
+    'alias cssl+'   value =>  'config --global http.sslVerify true';
+    'alias cssl-'   value =>  'config --global http.sslVerify false';
+}
+  
+}
