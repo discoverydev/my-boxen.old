598eb78ff3f98afa4f799756ec17a8459c5c973d
Discovery Dev <adsdiscoveryteam@pillartechnology.com>
2015-06-18 10:24:39 -0400

    adding logic to script in order to accept node name

--- start_jenkins_slave.sh
+++ start_jenkins_slave.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
+read -p "Enter node name: " NODE_NAME
+#NODE_NAME=${NODE_NAME:-null}
+
+echo $NODE_NAME
+
+if [ -z "$NODE_NAME" ]; 
+then 
+  echo "NODE_NAME is unset or set to the empty string";
+  exit 1;
+else 
+  echo "NODE_NAME is set to '$NODE_NAME'"; 
+fi
+
 echo "* starting jenkins slave"
-javaws http://192.168.8.4:8080/computer/osxhost/slave-agent.jnlp 2>&1
+javaws http://192.168.8.4:8080/computer/$NODE_NAME/slave-agent.jnlp
