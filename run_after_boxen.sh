cd4973fa55bdfc67c33f38431234fc2c94718904
Discovery Dev <adsdiscoveryteam@pillartechnology.com>
2015-06-18 14:42:08 -0400

    adding copy of 'tailored' android-sdk to machine

--- run_after_boxen.sh
+++ run_after_boxen.sh
@@ -6,3 +6,7 @@ npm install -g appium
 echo "* installing required gems"
 sudo gem install appium_console
 sudo gem install ocunit2junit
+
+echo "* copy 'tailored' adroid-sdk from master"
+cd /opt/boxen/homebrew/Cellar/android-sdk/
+scp -r admin@192.168.8.4:/opt/boxen/homebrew/Cellar/android-sdk/tailored .
