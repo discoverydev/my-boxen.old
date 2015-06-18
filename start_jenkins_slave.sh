fb0741c367391122009dcaba95d981c09f7fb6a7
Discovery Dev <adsdiscoveryteam@pillartechnology.com>
2015-06-18 12:39:51 -0400

    updates to script

--- start_jenkins_slave.sh
+++ start_jenkins_slave.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
+read -p "Enter node name: " NODE_NAME
+
+if [ -z ${NODE_NAME} ]; 
+then 
+  echo "NODE_NAME is unset"; 
+  exit 1;
+else 
+  echo "NODE_NAME is set to '$NODE_NAME'"; 
+fi
+
 echo "* starting jenkins slave"
-javaws http://192.168.8.4:8080/computer/osxhost/slave-agent.jnlp 2>&1
+javaws http://192.168.8.4:8080/computer/$NODE_NAME/slave-agent.jnlp 2>&1
