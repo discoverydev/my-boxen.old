9c22916a5f85ea889c8dd0806957b69a5a58be0f
Discovery Dev <adsdiscoveryteam@pillartechnology.com>
2015-06-26 10:42:56 -0400

    add requires stash host to ramrod projects

--- modules/projects/manifests/ramrod.pp
+++ modules/projects/manifests/ramrod.pp
@@ -1,17 +1,45 @@
 class projects::ramrod {
 	
-
   # ramrod android projects
-  boxen::project { 'ramrod_android_core':     source => 'http://stash/scm/mls/ramrod_android_core.git' }
-  boxen::project { 'ramrod_android_foo':      source => 'http://stash/scm/mls/ramrod_android_foo.git' }
-  boxen::project { 'ramrod_android_bar':      source => 'http://stash/scm/mls/ramrod_android_bar.git' }
-  boxen::project { 'ramrod_android_shell':    source => 'http://stash/scm/mls/ramrod_android_shell.git' }
+  boxen::project { 'ramrod_android_core':     
+    source => 'http://stash/scm/mls/ramrod_android_core.git',
+    require => Host['stash'] 
+  }
+  boxen::project { 'ramrod_android_foo':      
+    source => 'http://stash/scm/mls/ramrod_android_foo.git', 
+    require => Host['stash'] 
+  }
+  boxen::project { 'ramrod_android_bar':      
+    source => 'http://stash/scm/mls/ramrod_android_bar.git', 
+    require => Host['stash'] 
+  }
+  boxen::project { 'ramrod_android_shell':    
+    source => 'http://stash/scm/mls/ramrod_android_shell.git', 
+    require => Host['stash'] 
+  }
+
   # ramrod ios projects
-  boxen::project { 'ramrod_ios_core':         source => 'http://stash/scm/mls/ramrod_ios_core.git' }
-  boxen::project { 'ramrod_ios_foo':          source => 'http://stash/scm/mls/ramrod_ios_foo.git' }
-  boxen::project { 'ramrod_ios_bar':          source => 'http://stash/scm/mls/ramrod_ios_bar.git' }
-  boxen::project { 'ramrod_ios_shell':        source => 'http://stash/scm/mls/ramrod_ios_shell.git' }
+  boxen::project { 'ramrod_ios_core':         
+    source => 'http://stash/scm/mls/ramrod_ios_core.git', 
+    require => Host['stash'] 
+  }
+  boxen::project { 'ramrod_ios_foo':          
+    source => 'http://stash/scm/mls/ramrod_ios_foo.git', 
+    require => Host['stash'] 
+  }
+  boxen::project { 'ramrod_ios_bar':          
+    source => 'http://stash/scm/mls/ramrod_ios_bar.git', 
+    require => Host['stash'] 
+  }
+  boxen::project { 'ramrod_ios_shell':        
+    source => 'http://stash/scm/mls/ramrod_ios_shell.git', 
+    require => Host['stash'] 
+  }
+
   # ramrod functional tests
-  boxen::project { 'ramrod-functional-tests': source => 'http://stash/scm/mls/ramrod-functional-tests.git' }
+  boxen::project { 'ramrod-functional-tests': 
+    source => 'http://stash/scm/mls/ramrod-functional-tests.git', 
+    require => Host['stash'] 
+  }
 	
 }
