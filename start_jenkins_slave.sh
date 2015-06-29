638ae2f3543c081ebd5690f9f5d437af334f73f3
Discovery Dev <adsdiscoveryteam@pillartechnology.com>
2015-06-29 11:27:51 -0400

    removing requirement to provide hostname

--- start_jenkins_slave.sh
+++ start_jenkins_slave.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
 
-read -p "Enter node name: " NODE_NAME
-
-if [ -z ${NODE_NAME} ]; 
-then 
-  echo "NODE_NAME is unset"; 
-  exit 1;
-else 
-  echo "NODE_NAME is set to '$NODE_NAME'"; 
-fi
-
 echo "* starting jenkins slave"
-javaws http://jenkins/computer/$NODE_NAME/slave-agent.jnlp
+javaws http://jenkins/computer/`hostname -s`/slave-agent.jnlp
