5feb8fe79d1f5b6d1fedea2f00ab25ce5e4879a8
Discovery Dev <adsdiscoveryteam@pillartechnology.com>
2015-07-01 13:58:03 -0400

    [00000] ddaugher - adding export in order to restart docker

--- restart_confluence_machine.sh
+++ restart_confluence_machine.sh
@@ -13,6 +13,9 @@ progress_bar() {
 progress_bar
 
 export PATH=/opt/boxen/homebrew/bin:$PATH
+export DOCKER_HOST=tcp://192.168.59.103:2376
+export DOCKER_CERT_PATH=/Users/ga-mlsdiscovery/.boot2docker/certs/boot2docker-vm
+export DOCKER_TLS_VERIFY=1
 
 echo "* Restarting Confluence machine"
 echo "** shutting boot2docker down"
