c482ab5a5bd3d97ea0ee6d4bd8262d595434f6e0
Discovery Dev <adsdiscoveryteam@pillartechnology.com>
2015-07-15 16:06:14 -0400

    Whoops: directory path too long

--- manifests/scripts/set-up-dynatrace-adk.sh
+++ manifests/scripts/set-up-dynatrace-adk.sh
@@ -5,7 +5,7 @@ if ! [[ -d /opt/dynatrace/Android/auto-instrumentor ]]; then
 	unzip /tmp/dynatrace/agent\?version=6.2\&techtype=mobile -d /tmp/dynatrace/extract
 
 	rm -rf /opt/dynatrace/Android/auto-instrumentor
-	mkdir -p /opt/dynatrace/Android/auto-instrumentor
-	mv /tmp/dynatrace/extract/Android/auto-instrumentor /opt/dynatrace/Android/auto-instrumentor
+	mkdir -p /opt/dynatrace/Android
+	mv /tmp/dynatrace/extract/Android/auto-instrumentor /opt/dynatrace/Android/
 	rm -r /tmp/dynatrace
 fi
