c499fb90513e3bf452a4cf8cf76e250dbd7083d9
Discovery Dev <adsdiscoveryteam@pillartechnology.com>
2015-07-23 14:43:47 -0400

    ImageMagick version number is now separated

--- manifests/scripts/install_imagemagick_fonts.sh
+++ manifests/scripts/install_imagemagick_fonts.sh
@@ -4,7 +4,8 @@ FONT_DIRS="/System/Library/Fonts /Library/Fonts"
 
 TYPE_GEN="/opt/boxen/repo/manifests/scripts/type_gen"
 
-IMAGE_MAGICK_HOME="/opt/boxen/homebrew/Cellar/imagemagick/6.9.1-9"
+IMAGE_MAGICK_VERSION="6.9.1-9"
+IMAGE_MAGICK_HOME="/opt/boxen/homebrew/Cellar/imagemagick/$IMAGE_MAGICK_VERSION"
 IM_CONFIG="$IMAGE_MAGICK_HOME/etc/ImageMagick-6"
 IM_SYSTEM_TYPE_XML="$IM_CONFIG/type.xml"
 IM_LOCAL_TYPE_XML="$IM_CONFIG/local-type.xml"
