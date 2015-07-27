df6e99f963ff01b8dfc81d91dbf8040d937461bb
Discovery Dev <adsdiscoveryteam@pillartechnology.com>
2015-07-27 15:55:57 -0400

    dj.daugherty - adding OS specific nodes to startup script

--- start_jenkins_slave.sh
+++ start_jenkins_slave.sh
@@ -2,3 +2,7 @@
 
 echo "* starting jenkins slave"
 javaws http://jenkins/computer/`hostname -s`/slave-agent.jnlp
+echo "* starting jenkins IOS slave"
+javaws http://jenkins/computer/`hostname -s`_IOS/slave-agent.jnlp
+echo "* starting jenkins ANDROID slave"
+javaws http://jenkins/computer/`hostname -s`_ANDROID/slave-agent.jnlp
