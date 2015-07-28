c1116dd1249788ac8f9031b6caa4c3c0f84fe4b5
Discovery Dev <adsdiscoveryteam@pillartechnology.com>
2015-07-28 10:22:24 -0400

    jason.duffy, frank.mcauley, joey.wessel - add root and intermediate ca certs

--- modules/people/manifests/discoverydev.pp
+++ modules/people/manifests/discoverydev.pp
@@ -9,8 +9,20 @@ class people::discoverydev {
     source => "${boxen::config::repodir}/manifests/files/profile"
   }
 
-file { "/opt/boxen/homebrew/Cellar/sonar-runner/2.4/libexec/conf/sonar-runner.properties":
+  file { "/opt/boxen/homebrew/Cellar/sonar-runner/2.4/libexec/conf/sonar-runner.properties":
     require => Package['sonar-runner'],
     source => "${boxen::config::repodir}/manifests/files/sonar-runner.properties"
+  }
+
+  exec { "add-root-ca":
+  	command => 'security add-trusted-cert -d -r trustRoot -k "/Library/Keychains/System.keychain" "/opt/boxen/repo/manifests/files/ADSCorporate-Root-CA.cer"',
+  	path    => "/usr/local/bin/:/bin/:/usr/bin/",
+  	user    => root,
+  }
+
+  exec { "add-retail-ca":
+  	command => 'security add-trusted-cert -d -r trustRoot -k "/Library/Keychains/System.keychain" "/opt/boxen/repo/manifests/files/ADSRetail-Issuing-CA.cer"',
+  	path    => "/usr/local/bin/:/bin/:/usr/bin/",
+  	user    => root,
   }
 }
