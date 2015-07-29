943568f98b382c36063ea33070b57414a6fccdfa
Discovery Dev <adsdiscoveryteam@pillartechnology.com>
2015-07-29 15:55:59 -0400

    dj.daugherty - update to push to jenkins

--- update_boxen.sh
+++ update_boxen.sh
@@ -1,14 +1,23 @@
 #!/bin/bash -l
 
-echo $PATH
-
-. /opt/boxen/env.sh
-env
 echo "updating boxen"
 
+set +e
 cd /opt/boxen/repo
-git pull
 git checkout ads
+git pull
 ./script/boxen
 
-. /opt/boxen/env.sh
+RESULT=$?
+echo $RESULT
+
+if [ $RESULT -eq 2 ] || [ $RESULT -eq 6 ] || [ $RESULT -eq 4 ] || [ $RESULT -eq 0 ]
+then
+    echo "success"
+    curl http://jenkins/job/Boxen_$(hostname -s)/buildWithParameters?CALLER=BOXEN
+    . /opt/boxen/env.sh
+else
+    echo "failure"
+    exit 1
+fi
+
