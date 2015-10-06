a6ae4371b5072567e4cd004821394f2c07e0d6dc
Discovery Dev <adsdiscoveryteam@pillartechnology.com>
2015-10-06 14:21:12 -0400

    KS, DJ - jenins and stash scripts updated

--- stash-vm.sh
+++ stash-vm.sh
@@ -11,16 +11,17 @@ init() {
 	stop
 	create
 	nat ${DOCKER_CONTAINER}-http 7990 7990
+	nat ${DOCKER_CONTAINER}-ssh 7999 7999
 	start
 
 	DATA_DIR=/Users/Shared/data/$DOCKER_CONTAINER
 
 	echo "** docker $DOCKER_CONTAINER startup"
 	mkdir -p $DATA_DIR
-    docker run --name=stash --add-host nexus:192.168.8.31 --add-host jenkins:192.168.8.31 --add-host confluence:192.168.8.34 -d -v $DATA_DIR/stash:/var/atlassian/application-data/stash -p 7990:7990 -p 7999:7999 atlassian/stash
+    docker run --name $DOCKER_CONTAINER --restart=always --add-host nexus:192.168.8.31 --add-host jenkins:192.168.8.31 --add-host confluence:192.168.8.34 -d -v $DATA_DIR:/var/atlassian/application-data/stash -p 7990:7990 -p 7999:7999 atlassian/stash
 	docker ps
 
-	echo "** open stash browser"
+	echo "** open $DOCKER_CONTAINER browser"
 	open http://localhost:7990/
 }
 
