349d94b99ac42bbb21d05b80909bbcfe333e1af4
Discovery Dev <adsdiscoveryteam@pillartechnology.com>
2015-11-09 13:09:51 -0500

    Remove postgres stuff from sonarqube-vm.sh and created sonardb-vm.sh for postgres

--- vm/sonarqube-vm.sh
+++ vm/sonarqube-vm.sh
@@ -2,14 +2,15 @@
 
 export DOCKER_IMAGE=discoverydev/sonarqube-docker
 export DOCKER_CONTAINER=sonarqube
-export DOCKER_CONTAINER_PUBLISH="--publish=9000:9000 --publish=9092:9092"
+export DOCKER_CONTAINER_DATA_DIR=/opt/db/sonarqube/
+export DOCKER_CONTAINER_PUBLISH="--publish=9000:9000 --publish=443:443" 
 export DOCKER_VM_CPUS=1
 
 source docker-vm_lib.sh
 
 _setupvm() {
 	nat ${DOCKER_CONTAINER}-http 9000 9000
-	nat ${DOCKER_CONTAINER}-http 9092 9092
+	nat ${DOCKER_CONTAINER}-http 443 443 
 }
 
 open() {
