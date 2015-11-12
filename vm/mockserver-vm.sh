f78f009fb493130c410f07703693bd2209fe348e
Discovery Dev <adsdiscoveryteam@pillartechnology.com>
2015-11-12 13:56:31 -0500

    dj - created mockserver script for mock-server docker container

---
+++ vm/mockserver-vm.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+export DOCKER_IMAGE=discoverydev/mock-server
+export DOCKER_CONTAINER=mockserver
+export DOCKER_CONTAINER_DATA_DIR=/var/local/discoverydev/mockserver
+export DOCKER_CONTAINER_PUBLISH="--publish=1080:1080 --publish=1090:1090"
+export DOCKER_CONTAINER_ENV="--env JAVA_OPTS=-Xmx2g"
+export DOCKER_VM_MEMORY=3072
+
+source docker-vm_lib.sh
+
+_setupvm() {
+	nat ${DOCKER_CONTAINER}-http 1080 1080
+	nat ${DOCKER_CONTAINER}-proxy 1090 1090
+}
+
+tail-log() {
+	tail -n 100 -f $DOCKER_HOST_DATA_DIR/logs/discovery-mockserver.log
+}
+
+open() {
+	/usr/bin/open "http://localhost:1080/"
+}
+
+for arg in "$@"; do $arg; done
