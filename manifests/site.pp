e4c63913a039826fb0ae36c9ec70f09b0b7c152d
Discovery Dev <adsdiscoveryteam@pillartechnology.com>
2016-02-12 13:06:16 -0500

    MLS-000 Coleman - added explanation comment about sshpass

--- manifests/site.pp
+++ manifests/site.pp
@@ -163,7 +163,7 @@ node default {
       'wget',              # get things from the web (alternative to curl)
       'xctool',            # xcode build, used by sonar
       'carthage',          # xcode dependency management
-      'https://raw.githubusercontent.com/kadwanev/bigboybrew/master/Library/Formula/sshpass.rb' # sshpass  <-- duh, but what is it used for????
+      'https://raw.githubusercontent.com/kadwanev/bigboybrew/master/Library/Formula/sshpass.rb' # sshpass - used for piping passwords into ssh commands. it is MUCH better to set up a keypair. ask coleman if you don't know how.
     ]: 
     ensure => present,
     require => Exec['tap-discoverydev-ipa'],
